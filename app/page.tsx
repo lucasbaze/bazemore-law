2977d1471827a5063ffec581ca831a387050e72c
lucas <lucasabazemore@gmail.com>
2024-08-30 17:22:31 -0500

    most styling done

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 
 const services = [
   {
-    image: '/older-couple.jpg',
-    imageAlt: 'Older couple',
+    image: '/cow-girls.jpg',
+    imageAlt: 'Cow girls',
     title: 'Trusts',
     description:
       'A trust might be a valuable tool in planning for long term care or simply asset protection, but how do you know which trust is the best “fit” for your circumstances? Or, if you really need one at all?',
@@ -19,22 +19,22 @@ const services = [
       'Most families are not independently wealthy and nursing home costs continue to skyrocket. Navigating the gauntlet of Medicaid for the elderly without losing your life savings is daunting and requires a broad knowledge of the traps and pitfalls of the state Medicaid rules and system.',
   },
   {
-    image: '/older-couple.jpg',
-    imageAlt: 'Older couple',
+    image: '/families.jpg',
+    imageAlt: 'Families',
     title: 'Estate Planning',
     description:
       'Estate planning is more than just asking, “Where will my stuff go when I die?” More importantly, what happens to your family and responsibilities if you are incapacitated or need long term skilled care?',
   },
   {
-    image: '/older-couple.jpg',
-    imageAlt: 'Older couple',
+    image: '/windmills.jpg',
+    imageAlt: 'Windmills',
     title: 'Probate',
     description:
       'Settling the affairs of a loved one is often a tiring and thankless job. And, if you don’t have a Will, the State of Texas decides where your stuff will go. We can help plan to get it right and navigate the court process.',
   },
   {
-    image: '/older-couple.jpg',
-    imageAlt: 'Older couple',
+    image: '/cotton.jpg',
+    imageAlt: 'Cotton Fields',
     title: 'Series LLC',
     description:
       'A series LLC is a holding company that can segregate assets under independent series for liability protection without the cost and headache of setting up multiple companies (e.g. rent houses) and is a good solution to segregate trust assets from business assets for protection of future generations.',
@@ -63,14 +63,16 @@ export default function Home() {
               Attorney Robert Bazemore is a life-long entrepreneur with over 30
               years combined experience in business, healthcare and law.
             </p>{' '}
-            <div>Call For Consultation: (806) 994-2060</div>
+            <div>
+              <Button>Call For Consultation: (806) 994-2060</Button>
+            </div>
           </div>
         </div>
       </section>
 
       {/* Services */}
-      <section className="bg-gray-100 py-12">
-        <div className="container mx-auto text-center mb-8">
+      <section className="bg-gray-100 py-24">
+        <div className="container mx-auto text-center mb-16">
           <h2 className="text-4xl font-bold mb-2">Our Law Services</h2>
           <p className="text-gray-600 font-sans text-2xl">
             Thoughtful legal planning for the Golden Years of life
@@ -90,12 +92,10 @@ export default function Home() {
                   height={300}
                   className="w-full"
                 />
-                <div className="absolute -bottom-4 left-1/2 text-nowrap transform -translate-x-1/2 bg-blue-800 text-white font-bold py-2 px-4 rounded-none text-xl">
-                  {service.title}
-                </div>
               </div>
-              <div className="px-8 py-12 font-sans">
-                <p className="text-gray-600">{service.description}</p>
+              <div className="p-8">
+                <div className="font-bold text-2xl pb-4">{service.title}</div>
+                <p className="text-gray-600 font-sans">{service.description}</p>
               </div>
             </div>
           ))}
@@ -103,7 +103,7 @@ export default function Home() {
       </section>
 
       {/* Section Separator */}
-      <section className="py-12">
+      <section className="py-24">
         <div className="w-full">
           <Image
             src="/cowboys.jpg"
@@ -121,10 +121,10 @@ export default function Home() {
       </section>
 
       {/* Profile Info */}
-      <section className="bg-gray-100 py-12">
-        <div className="container mx-auto flex flex-col md:flex-row items-center">
+      <section className="bg-gray-100 py-24">
+        <div className="container max-w-6xl mx-auto flex flex-col md:flex-row items-stretch">
           {/* Image Section */}
-          <div className="w-full md:w-1/3">
+          <div className="w-full">
             <Image
               src="/bazemore-full-shot.jpg"
               alt="Robert Bazemore, Esq."
@@ -135,44 +135,61 @@ export default function Home() {
           </div>
 
           {/* Text Section */}
-          <div className="w-full md:w-1/2 bg-white p-8 md:p-12">
-            <h2 className="text-3xl font-bold mb-4">Robert Bazemore, Esq.</h2>
-            <hr className="w-16 border-t-2 border-black mb-4" />
-            <p className="text-gray-700 leading-relaxed">
-              Robert is the founder of Bazemore Law Firm PLLC. He holds a degree
-              in business administration from LeTourneau University and a
-              doctorate degree in law from South Texas College of Law – Houston.
-              He is accredited by the Veterans Administration and is a member of
-              the National Academy of Elder Law Attorneys, Elder Counsel, the
-              State Bar of Texas – Real Estate, Probate & Trust Law Section,
-              Elder Law Answers, and multiple other relevant associations. He
-              was awarded a Presidential Management Fellowship in Washington,
-              D.C. after law school.
-            </p>
+          <div className="w-full bg-white p-8 md:p-12 flex flex-row items-center">
+            <div className="">
+              <h2 className="text-3xl font-bold mb-4">Robert Bazemore, Esq.</h2>
+              <hr className="w-16 border-t-2 border-black mb-4" />
+              <p className="text-gray-700 leading-relaxed">
+                Robert is the founder of Bazemore Law Firm PLLC. He holds a
+                degree in business administration from LeTourneau University and
+                a doctorate degree in law from South Texas College of Law –
+                Houston. He is accredited by the Veterans Administration and is
+                a member of the National Academy of Elder Law Attorneys, Elder
+                Counsel, the State Bar of Texas – Real Estate, Probate & Trust
+                Law Section, Elder Law Answers, and multiple other relevant
+                associations. He was awarded a Presidential Management
+                Fellowship in Washington, D.C. after law school.
+              </p>
+            </div>
           </div>
         </div>
       </section>
 
       {/* Map */}
-      <section className="bg-white p-8">
-        <div className="container mx-auto text-center">
+      <section className="bg-white pt-24 pb-12 max-w-5xl mx-auto">
+        <h2 className="text-3xl font-semibold mb-8 leading-relaxed text-center">
+          Get in Touch
+        </h2>
+        <div className="flex">
           {/* <!-- Address and Contact Information --> */}
-          <div className="mb-8">
-            <h2 className="text-2xl font-semibold">
-              4710 67th Street, Suite F • Lubbock, Texas 79414
-            </h2>
-            <p className="text-lg">Phone (806) 329-4601 | Fax (806) 370-3732</p>
+          <div className="w-1/3">
+            <h3 className="font-semibold text-xl mb-4">Contact Us</h3>
+            <div className="font-sans mb-8">
+              <p className="mb-2">Bazemore Law Firm PLLC</p>
+              <p className="mb-2">
+                4710 67th Street, <br />
+                Suite F Lubbock, Texas 79414
+              </p>
+              <p className="mb-2">Office | (806) 329-4601</p>
+              <p className="mb-2">Fax | (806) 370-3732</p>
+              <p className="mb-2">bazemorelaw@gmail.com</p>
+            </div>
+            <h3 className="font-semibold text-xl mb-2">Office Hours:</h3>
+            <p className="font-sans">
+              <i>By appointment only</i>
+            </p>
           </div>
-
-          {/* <!-- Google Map Embed --> */}
-          <div className="flex justify-center">
-            <iframe
-              className="w-full md:w-3/4 lg:w-1/2 h-80 border-0"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.8351475478676!2d-101.93148352436686!3d33.53049063478244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86fe0b9dbdd4031f%3A0xb6f594f914f4972e!2s4710%2067th%20St%2C%20Lubbock%2C%20TX%2079414%2C%20USA!5e0!3m2!1sen!2sin!4v1695993333445!5m2!1sen!2sin"
-              allowFullScreen={false}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+          <div className="w-2/3 container mx-auto text-center">
+            {/* <!-- Google Map Embed --> */}
+            <div className="flex justify-stretch" style={{ height: '500px' }}>
+              <iframe
+                className="w-full lg:max-w-6xl h-full border-0 mx-auto"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3168.8351475478676!2d-101.93148352436686!3d33.53049063478244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86fe0b9dbdd4031f%3A0xb6f594f914f4972e!2s4710%2067th%20St%2C%20Lubbock%2C%20TX%2079414%2C%20USA!5e0!3m2!1sen!2sin!4v1695993333445!5m2!1sen!2sin"
+                allowFullScreen={false}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
       </section>
--- components/mainFooter.tsx
+++ components/mainFooter.tsx
@@ -1,40 +1,58 @@
 import { Flower2 } from 'lucide-react';
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 
 const MainFooter = () => {
   return (
     <div className="mt-10 shadow-md">
-			<footer className="bg-blue-800 text-white p-8">
-  <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
-    {/* <!-- Logo and Name --> */}
-    <div className="flex flex-col items-center md:items-start">
-      <img src="/bazemore-logo-white.png" alt="Bazemore Law Firm Logo" className="mb-4 w-24 md:w-32" />
-      <h2 className="text-2xl font-bold">Bazemore Law Firm, PLLC</h2>
-    </div>
+      <footer className="bg-blue-800 text-white py-16 px-8">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
+          {/* <!-- Logo and Name --> */}
+          <div className="flex flex-col items-center md:items-start">
+            <img
+              src="/bazemore-logo-white.png"
+              alt="Bazemore Law Firm Logo"
+              className="mb-4 w-36 md:w-52"
+            />
+          </div>
 
-    {/* <!-- Location Details --> */}
-    <div className="flex flex-col items-center md:items-start">
-      <h3 className="text-lg font-semibold uppercase mb-2">Our Location</h3>
-      <p>Bazemore Law Firm PLLC</p>
-      <p>4710 67th Street, Suite F</p>
-      <p>Lubbock, Texas 79414</p>
-      <p>Phone: (806) 329-4601</p>
-      <p>Fax: (806) 370-3732</p>
-      <a href="https://maps.google.com" className="text-white font-semibold mt-2">get directions</a>
-    </div>
+          {/* <!-- Location Details --> */}
+          <div className="flex flex-col items-center md:items-start">
+            <h3 className="text-lg font-semibold uppercase mb-2">
+              Our Location
+            </h3>
+            <p className="font-sans">Bazemore Law Firm PLLC</p>
+            <p className="font-sans">4710 67th Street, Suite F</p>
+            <p className="font-sans">Lubbock, Texas 79414</p>
+            <p className="font-sans">Phone: (806) 329-4601</p>
+            <p className="font-sans mb-4">Fax: (806) 370-3732</p>
+            <a
+              href="https://maps.google.com"
+              className="text-white font-semibold mb-8 underline font-sans"
+            >
+              Get Directions
+            </a>
+            <a href="/pay-invoice" className="text-white text-lg font-semibold">
+              PAY INVOICE
+            </a>
+          </div>
 
-    {/* <!-- Affiliations and Invoice --> */}
-    <div className="flex flex-col items-center md:items-start">
-      <h3 className="text-lg font-semibold uppercase mb-2">Affiliations</h3>
-      <p>Texas Bar Association</p>
-      <img src="/bazemore-logo-white.png" alt="Elder Law Answers" className="mt-4 mb-4 w-24 md:w-32" />
-      <a href="/pay-invoice" className="text-white text-lg font-semibold">PAY INVOICE</a>
-    </div>
-  </div>
-</footer>
+          {/* <!-- Affiliations and Invoice --> */}
+          <div className="flex flex-col items-center md:items-start">
+            <h3 className="text-lg font-semibold uppercase mb-2">
+              Affiliations
+            </h3>
+            <p className="font-sans mb-6">Texas Bar Association</p>
+            <img
+              src="/ela-badge-wht.png"
+              alt="Elder Law Answers"
+              className="mt-4 mb-4 w-24 md:w-28"
+            />
+          </div>
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
-export default MainFooter
+export default MainFooter;
